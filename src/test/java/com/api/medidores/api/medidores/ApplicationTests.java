1218d145093c068bcec5c035a12418fe8395ca6f
aminLuis <aminluisamin@gmail.com>
2023-01-03 22:09:49 -0500

    Inicion API medidores - Configuracion application.properties

---
+++ src/test/java/com/api/medidores/api/medidores/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.medidores.api.medidores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
